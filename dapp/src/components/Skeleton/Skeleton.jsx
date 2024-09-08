2e98a4c291785e451f604a329631fe89e436e68a
chidal <chidianachebie@gmail.com>
2024-09-08 18:13:44 +0100

    pre constant

--- dapp/src/components/Skeleton/Skeleton.jsx
+++ dapp/src/components/Skeleton/Skeleton.jsx
@@ -1,3 +1,22 @@
-export { default as Loader } from "./Loader";
-export { default as Exchange } from "./Exchange";
-export { default as WalletButton } from "./WalletButton";
+import styles from './styles.module.css'
+
+const Skeleton = ({count=1, type='banner'}) => {
+  return (
+    <>
+      {count > 1 ? (
+        <ul className={styles.list}>
+          {[...Array(count)].map((_, index) => (
+            <li
+              key={index}
+              className={type === "banner" ? styles.banner : styles.item}
+            ></li>
+          ))}
+        </ul>
+      ) : (
+        <li className={type === "banner" ? styles.banner : styles.item}></li>
+      )}
+    </>
+  )
+}
+
+export default Skeleton
