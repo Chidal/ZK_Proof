133199262b6e86f33f88e3cdfc34dac96fc566e1
chidal <chidianachebie@gmail.com>
2024-09-08 17:53:40 +0100

    image rendering

--- dapp/src/components/Header/Header.jsx
+++ dapp/src/components/Header/Header.jsx
@@ -1,67 +1,13 @@
-import React, { useState, useEffect, useRef } from "react";
-import { formatUnits } from "ethers/lib/utils";
+import { formatDate } from '../../helpers/formatDate'
+import styles from './styles.module.css'
 
-import { chevronDown } from "../assets";
-import { useAmountsOut, useOnClickOutside } from "../utils";
-import styles from "../styles";
-
-const AmountOut = ({ fromToken, toToken, amountIn, pairContract, currencyValue, onSelect, currencies }) => {
-  const [showList, setShowList] = useState(false);
-  const [activeCurrency, setActiveCurrency] = useState("Select");
-  const ref = useRef()
-
-  const amountOut = useAmountsOut(pairContract, amountIn, fromToken, toToken) ?? 0;
-
-  useOnClickOutside(ref, () => setShowList(false))
-
-  useEffect(() => {
-    if (Object.keys(currencies).includes(currencyValue)) {
-      setActiveCurrency(currencies[currencyValue]);
-    } else {
-      setActiveCurrency("Select")
-    }
-  }, [currencyValue, currencies]);
-
+const Header = () => {
   return (
-    <div className={styles.amountContainer}>
-      <input
-        placeholder="0.0"
-        type="number"
-        value={formatUnits(amountOut)}
-        className={styles.amountInput}
-        disabled
-      />
+    <header className={styles.header}>
+        <h1 className={styles.title}>World News</h1>
+        <p className={styles.date}>{formatDate(new Date())}</p>
+    </header>
+  )
+}
 
-      <div className="relative" onClick={() => setShowList(!showList)}>
-        <button className={styles.currencyButton}>
-          {activeCurrency}
-          <img
-            src={chevronDown}
-            alt="cheveron-down"
-            className={`w-4 h-4 object-contain ml-2 ${showList ? "rotate-180" : "rotate-0"}`}
-          />
-        </button>
-
-        {showList && (
-          <ul ref={ref} className={styles.currencyList}>
-            {Object.entries(currencies).map(([token, tokenName], index) => (
-              <li
-                key={index}
-                className={styles.currencyListItem}
-                onClick={() => {
-                  if (typeof onSelect === "function") onSelect(token);
-                  setActiveCurrency(tokenName);
-                  setShowList(false);
-                }}
-              >
-                {tokenName}
-              </li>
-            ))}
-          </ul>
-        )}
-      </div>
-    </div>
-  );
-};
-
-export default AmountOut;
+export default Header
